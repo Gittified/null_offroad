f911d14175610417718f91126418b92a6771d0d6
Nullified <28195796+Gittified@users.noreply.github.com>
2022-10-13 21:33:35 +0200

    feat(config): add more roads

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.UpdateCheck = true -- Check for update on resource start?
 
 Config.BypassVehicles = {} -- Offroad vehicles that should bypass the script, use the model name e.g.: 'adder'
-Config.GripRoads = { 1, 4, 3, 7, 181, 15, 13, 55, 68, 69, 12 }
+Config.GripRoads = { 1, 4, 3, 7, 181, 15, 13, 55, 68, 69, 12, 31, 36, 35, 173, 64 }
 Config.OffroadClass = { 9 }
 
 -- If you enable the following option, please go to the fxmanifest and uncomment the oxmysql script.
