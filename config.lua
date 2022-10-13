1d1e108375286542834cef3df519ac4140f9a74f
Nullified <28195796+Gittified@users.noreply.github.com>
2022-10-13 15:59:56 +0200

    feat(config): Added option to enable oxmysql integration
    Signed-off-by: Nullified <28195796+Gittified@users.noreply.github.com>

--- config.lua
+++ config.lua
@@ -4,6 +4,8 @@ Config.UpdateCheck = true -- Check for update on resource start?
 Config.GripRoads = { 1, 4, 3, 7, 181, 15, 13, 55, 68, 69, 12 }
 Config.OffroadClass = { 9 }
 
+Config.EnableSQL = false -- OxMySQL only! Ability to add/remove vehicles to the bypass list without having to restart.
+
 -- Client side notifications!
 Config.Notification = function(message)
     TriggerEvent('chat:addMessage', {
