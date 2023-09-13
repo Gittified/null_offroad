c7f374000a916d7a091a6362c644e0dc8e0ab1ce
Nullified <28195796+Gittified@users.noreply.github.com>
2023-09-13 11:23:00 +0200

    feat(config): added some additional material types
    Signed-off-by: Nullified <28195796+Gittified@users.noreply.github.com>

--- config.lua
+++ config.lua
@@ -13,7 +13,7 @@ Config = {}
 Welcome to the configuration section of null_offroad, the variables below should not be edited unless
 you know what you are doing. If you edit them and something breaks it is your own fault. The configuration
 options are explained well and you should be able to easily configure the script. Please send me a message
-if anything goes wrong or if you have questions about the configuration of the script. 
+if anything goes wrong or if you have questions about the configuration of the script.
 --]]
 
 Config.UpdateCheck = true -- Checks for updates on resource startup
@@ -54,10 +54,10 @@ Config.BypassVehicles = {}
 Config.BypassVehicleClasses = { 9 }
 
 -- Material types that should not cause any grip loss. You can find these numbers in the debugger.
-Config.Roads = { 1, 4, 3, 7, 181, 15, 13, 55, 68, 69, 12, 31, 36, 35, 173, 64 }
+Config.Roads = { 0, 1, 4, 3, 7, 181, 15, 13, 55, 68, 69, 12, 31, 36, 35, 173, 64, 70, 86 }
 
 -- This will multiply the intensity of the effect that is applied when a vehicle goes off-road. The default is 1 for none.
--- The effect of this variable can be pretty big, and there are also max values from where it will completely break. 
+-- The effect of this variable can be pretty big, and there are also max values from where it will completely break.
 -- Properly test your configuration!
 Config.IntensityMultiplier = 1
 
